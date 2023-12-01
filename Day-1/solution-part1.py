ab660565d67afc77bdbceff594301f2c08c5790a
Eric Gu <ericgu@erics-mbp.myfiosgateway.com>
2023-12-01 15:58:06 -0500

    Day 1

---
+++ Day-1/solution-part1.py
@@ -0,0 +1,19 @@
+input = open('input.txt', 'r')
+lines = input.readlines()
+ 
+sum = 0
+for line in lines:
+    firstDigit = 0
+    lastDigit = len(line) - 1
+    while firstDigit < len(line):
+        if line[firstDigit].isnumeric():
+            break
+        else:
+            firstDigit += 1
+    while lastDigit > 0:
+        if line[lastDigit].isnumeric():
+            break
+        else:
+            lastDigit -= 1
+    sum += int(line[firstDigit] + line[lastDigit])
+print(sum)
